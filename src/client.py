aa6b52e303b3914f0cd8f52e046779a710b22980
DVoronkin <vd@x-lab.com.ru>
2023-10-24 22:23:48 +0500

    Created simple client and server to test binary data sending

--- src/client.py
+++ src/client.py
@@ -1,1 +1,22 @@
 """HTTP client."""
+import asyncio
+import aiohttp
+
+
+async def main() -> None:
+    """Send simple binary data."""
+    url = "http://0.0.0.0:8080/file/"
+    file_path = "tests/data/Lenna.png"
+    async with aiohttp.ClientSession() as s:
+        data = aiohttp.FormData()
+        with open(file_path, "rb") as f:
+            data.add_field(
+                name="file",
+                value=f
+            )
+            async with s.post(url, data=data) as r:
+                print(r.status)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
