51043a7f967db6d7d176d72ea76bf2b35590a972
PearlFoe <57188977+PearlFoe@users.noreply.github.com>
2023-11-01 22:10:19 +0500

    Added some unit tests (#5)
    * Initial commit
    
    * Set project folders, code checker and project cofig
    
    * Created simple client and server to test binary data sending
    
    * Moved examples, created deploy instruction, dumped requirements
    
    * Set up routing structure, created new sub app.
    
    Also:
    - modified .gitignore
    - created project settings model
    - updated tests
    
    * Updated server logic
    
    * Deleted useless test file
    
    * Added some unit tests
    
    ---------
    
    Co-authored-by: DVoronkin <vd@x-lab.com.ru>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import pytest
 import pytest_asyncio
 from aiohttp.test_utils import TestServer, TestClient
@@ -10,8 +11,41 @@ from src.settings import Settigns
 
 
 @pytest.fixture(scope="session")
+def event_loop() -> asyncio.AbstractEventLoop:
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
 def settings() -> Settigns:
     return Settigns()
+
+
+@pytest.fixture(scope="session")
+def container(settings: Settigns) -> containers.Container:
+    container = containers.Container()
+    container.env.from_dict(settings.model_dump())
+    return container
+
+
+@pytest_asyncio.fixture(scope="function")
+async def application(file_handler_mock: FileHandler) -> web.Application:
+    server_app = await app.get_app()
+    server_app.container.file_handler.override(file_handler_mock)
+
+    return server_app
+
+
+@pytest_asyncio.fixture(scope="function")
+async def server(aiohttp_server: TestServer, application: web.Application) -> web.Server:
+    return await aiohttp_server(application)
+
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client(aiohttp_client: TestClient, application: web.Application) -> ClientSession:
+    return await aiohttp_client(application)
 
 
 @pytest.fixture(scope="class")
@@ -31,20 +65,5 @@ def file_handler_mock(settings: Settigns) -> FileHandler:
 
 
 @pytest.fixture(scope="class")
-def container(settings: Settigns) -> containers.Container:
-    container = containers.Container()
-    container.env.from_dict(settings.model_dump())
-    return container
-
-
-@pytest_asyncio.fixture(scope="function")
-async def server(aiohttp_server: TestServer) -> web.Server:
-    server_app = await app.get_app()
-    return await aiohttp_server(server_app)
-
-
-
-@pytest_asyncio.fixture(scope="function")
-async def client(aiohttp_client: TestClient) -> ClientSession:
-    server_app = await app.get_app()
-    return await aiohttp_client(server_app)
+def file_handler(settings: Settigns) -> FileHandler:
+    return FileHandler(storage_dir=settings.storage_dir)
--- tests/unit/storage/test_download.py
+++ tests/unit/storage/test_download.py
@@ -1,9 +1,33 @@
-from aiohttp import ClientSession
+from aiohttp import ClientSession, MultipartReader
 
 
-async def test_download(client: ClientSession) -> None:
+from src.server.storage.file_handlers import FileHandler
+
+
+async def test_validation_success(client: ClientSession) -> None:
     response = await client.post(
         "/api/v1/storage/download/",
         json={"file_name": "Lenna.png"}
     )
     assert response.status == 200
+
+
+async def test_validation_error(client: ClientSession) -> None:
+    response = await client.post(
+        "/api/v1/storage/download/",
+        json={}
+    )
+    assert response.status == 400
+
+
+async def test_binary_multupart_response(
+        client: ClientSession, file_handler_mock: FileHandler) -> None:
+    response = await client.post(
+        "/api/v1/storage/download/",
+        json={"file_name": "Lenna.png"}
+    )
+
+    reader = MultipartReader.from_response(response)
+    sub_reader =await reader.next()
+
+    assert await sub_reader.read() == await file_handler_mock._read("test_path")
